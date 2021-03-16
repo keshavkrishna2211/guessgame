b03839b27485e5e9bde562c0e4a25152fe89e749
Keshav <keshavkrishna2211@gmail.com>
2021-03-16 19:36:26 +0530

    change-theme

---
+++ playground.js
@@ -0,0 +1,78 @@
+// myName='Chris'
+// function logName()
+// {
+//   console.log(myName)
+// }
+// logName()
+// var myName
+// let myName='Keshav'
+// myName='sanju'
+// console.log(myName)
+// let lotsofdecimal=1.87655344644
+// lotsofdecimal;
+// console.log(lotsofdecimal)
+// lotsofdecimal=lotsofdecimal.toFixed(2)
+// console.log(lotsofdecimal)
+// let myNumber='3'
+// myNumber;
+// console.log(myNumber)
+// console.log((Number(myNumber)+3)**3)
+// let myNumber=3
+// console.log(myNumber++)
+// console.log(myNumber)
+// let myNumber=3
+//  console.log(++myNumber)
+//  console.log(myNumber)
+// let bigMouth='I\'ve got no right to take my place....'
+// console.log(bigMouth)
+// let song='Fight the youth'
+// let score=9;
+// let highestscore=10;
+// let output=`I like the song "${song}".I gave it a score of'${score/highestscore*100}'% .`
+// console.log(output)
+// let examScore=45;
+// let examHighestScore=70;
+//  examReport=`Your score ${examScore}/${examHighestScore}(${Math.round((examScore/examHighestScore*100))}%).\n${examScore>=49?'Congratulation you are passed':'Bad luck ! Try next time'}`
+//  console.log(examReport)
+// let shopping=['bread','milk','cheese','hummus','noodles']
+// console.log(shopping)
+// let random=['tree',795,[0,1,2]]
+// console.log(random)
+// let myData='Manchaster,london,Liverpool,Birmingham,Leeds,Carlisle'
+// let myArray=myData.split(',')
+// console.log(myData)
+// console.log(myArray)
+// console.log(myArray.length)
+// console.log(myArray[0])
+// console.log(myArray[1])
+// console.log(myArray[myArray.length-1])
+// console.log(myArray.join(','))
+// console.log(myArray.toString())
+// myArray.push('Cardiff')
+// console.log(myArray)
+// myArray.push('Bradford','Brington')
+// console.log(myArray)
+// let Length=myArray.push('Bristol')
+// console.log(myArray)
+// console.log(Length)
+// myArray.pop()
+// console.log(myArray)
+// myArray.unshift('New Delhi')
+// console.log(myArray)
+// myArray.shift()
+// console.log(myArray)
+// let products = ['Underpants:6.99',
+//   'Socks:5.99',
+//   'T-shirt:14.99',
+//   'Trousers:31.99',
+//   'Shoes:23.99']
+//   console.log(products)
+//   for(let i=0;i<products.length;i++)
+//   {
+//     let myArray=products[i].split(':')
+    
+//     let name=myArray[0]
+//     console.log(myArray)
+//     console.log(name)
+//   }
+
